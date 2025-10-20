9c17c68e7a578e00c1497944dc1ec0f941d7aad9
Sangay Choden <02240355.cst@rub.edu.bt>
2025-10-20 15:51:07 +0600

    manager update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './ManagerDashboard.jsx'
+import Profile from './ManagerProfile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Profile />
   </StrictMode>,
 )
