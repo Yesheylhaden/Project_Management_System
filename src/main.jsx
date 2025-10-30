6346ba591a4976d70b8fa34758b2733975606a8d
Sangay Choden <02240355.cst@rub.edu.bt>
2025-10-30 13:20:35 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import ForgotPassword from './forgotPassword.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import AdminDashboard from "./AdminDashboard.jsx";
 import AdminProfile from "./AdminProfile.jsx";
 import ManagerDashboard from "./ManagerDashboard.jsx";
 import ManagerProfile from "./ManagerProfile.jsx";
 import Signup from "./signup.jsx";
+import forgotPassword from "./forgotPassword.jsx";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ForgotPassword />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        {/* Default route shows ManagerProfile */}
+        <Route path="/" element={<Signup />} />
+        <Route path="managerProfile" element={<ManagerProfile />} />
+        <Route path="adminProfile" element={<AdminProfile />} />
+
+        {/* Dashboard route */}
+        <Route path="/managerDashboard" element={<ManagerDashboard />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/forgotPassword" element={<forgotPassword />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
