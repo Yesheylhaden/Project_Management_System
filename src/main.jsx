f42e3a62fe4ec1b5c18ebab21558a01986ca0e2f
Sangay Choden <02240355.cst@rub.edu.bt>
2025-10-30 13:07:38 +0600

    main chnage

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import AdminDashboard from "./AdminDashboard.jsx";
+import AdminProfile from "./AdminProfile.jsx";
 import ManagerDashboard from "./ManagerDashboard.jsx";
 import ManagerProfile from "./ManagerProfile.jsx";
+import Signup from "./signup.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        {/* Default route shows ManagerProfile */}
-        <Route path="/" element={<ManagerProfile />} />
+        {/* Default route shows profile */}
+        <Route path="/" element={<Signup />} />
+         <Route path="/adminProfile" element={<AdminProfile/>} />
+         <Route path="/managerProfile" element={<ManagerProfile />} />
 
         {/* Dashboard route */}
-        <Route path="/ManagerDashboard" element={<ManagerDashboard />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/managerDashboard" element={<ManagerDashboard />} />
+
       </Routes>
     </Router>
   </StrictMode>
