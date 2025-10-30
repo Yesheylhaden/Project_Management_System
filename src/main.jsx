c620103eef2b7bbec985ad663e18f82e7977f669
Sangay Choden <02240355.cst@rub.edu.bt>
2025-10-30 12:50:20 +0600

    admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./AdminDashboard.css";
 import "./AdminProfile.css";
+import "./signup.jsx"
+import "./signup.css"
 
 import AdminDashboard from "./AdminDashboard.jsx";
 import AdminProfile from "./AdminProfile.jsx";
-
+import Signup from "./signup.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         {/* Default route shows AdminProfile */}
         <Route path="/" element={<AdminProfile />} />
+        <Route path="/signup" element={<Signup />} />
 
         {/* Dashboard route */}
         <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="signup" element={<Signup />} />
       </Routes>
     </Router>
   </StrictMode>
